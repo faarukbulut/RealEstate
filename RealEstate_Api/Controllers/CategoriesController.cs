07f9e7e19a67d32fc94e0589b439e385e8142293
faarukbulut <faarukbulut@gmail.com>
2023-08-08 06:58:40 +0300

    Kategorilerin Apide Listelenmesi

---
+++ RealEstate_Api/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Api.Repositories.CategoryRepository;
+
+namespace RealEstate_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoyRepository;
+
+        public CategoriesController(ICategoryRepository categoyRepository)
+        {
+            _categoyRepository = categoyRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var values = await _categoyRepository.GetAllCategoryAsync();
+            return Ok(values);
+        }
+
+
+    }
+}
--- RealEstate_Api/Program.cs
+++ RealEstate_Api/Program.cs
@@ -1,9 +1,12 @@
+using RealEstate_Api.Models.DapperContext;
+using RealEstate_Api.Repositories.CategoryRepository;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddTransient<Context>();
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
