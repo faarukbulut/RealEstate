ab5d72420ceec7465bb9c15da0211a04c03759cc
faarukbulut <faarukbulut@gmail.com>
2023-08-22 19:32:31 +0300

    Anasayfa - layout componentler tamamlandı.

---
+++ RealEstate_UI/ViewComponents/DefaultLayout/_DefaultNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_UI.ViewComponents.DefaultLayout
+{
+    public class _DefaultNavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealEstate_UI/ViewComponents/DefaultLayout/_DefaultScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_UI.ViewComponents.DefaultLayout
+{
+    public class _DefaultScriptComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
