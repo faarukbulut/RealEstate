4c726d9556fbf775629d21dedb898cabab77d91f
faarukbulut <faarukbulut@gmail.com>
2023-11-03 07:42:03 +0300

    Who We Are Detail Sayfası Api Consume İşlemleri

--- RealEstate_Api/Controllers/WhoWeAreDetailController.cs
+++ RealEstate_Api/Controllers/WhoWeAreDetailController.cs
@@ -30,7 +30,7 @@ namespace RealEstate_Api.Controllers
             return Ok("Hakkımızda kısmı başarılı bir şekilde eklendi");
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteWhoWeAreDetail(int id)
         {
             _whoWeAreDetailRepository.DeleteWhoWeAreDetail(id);
