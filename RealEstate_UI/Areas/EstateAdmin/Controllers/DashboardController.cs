0f532a5c227f569d4c6d05b8ecd78c4a97ef7d6f
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-26 22:40:08 +0300

    Estate Admin Area

---
+++ RealEstate_UI/Areas/EstateAdmin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_UI.Areas.EstateAdmin.Controllers
+{
+    [Area("EstateAdmin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealEstate_UI/Areas/EstateAdmin/Controllers/ProductController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using RealEstate_UI.Dtos.CategoryDtos;
+using RealEstate_UI.Dtos.ProductDtos;
+
+namespace RealEstate_UI.Areas.EstateAdmin.Controllers
+{
+    [Area("EstateAdmin")]
+    public class ProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44367/api/Products/ProductListWithCategory");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateProduct()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44367/api/Categories/");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+
+            List<SelectListItem> categoryValues = (from x in values.ToList()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryID.ToString()
+                                                   }).ToList();
+
+            ViewBag.v = categoryValues;
+
+            return View();
+        }
+
+        public async Task<IActionResult> ProductDealOfTheDayStatusChangeOfToTrue(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44367/api/Products/ProductDealOfTheDayStatusChangeOfToTrue/" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> ProductDealOfTheDayStatusChangeOfToFalse(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44367/api/Products/ProductDealOfTheDayStatusChangeOfToFalse/" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+    }
+}
