424a4aca9af8e717d71b3089754a2ee2bf9b562e
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-26 20:34:36 +0300

    Jwt Web Token

---
+++ RealEstate_Api/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate_Api.Tools
+{
+    public class JwtTokenDefaults
+    {
+        public const string ValidAudience = "https://localhost";
+        public const string ValidIssuer = "https://localhost";
+        public const string Key = "ReaLEstate..01311acaca+131??aada__231++??axxx..--++NetCore666";
+        public const int Expire = 5;
+    }
+}
---
+++ RealEstate_Api/Tools/TokenResponseModel.cs
@@ -0,0 +1,14 @@
+﻿namespace RealEstate_Api.Tools
+{
+    public class TokenResponseModel
+    {
+        public TokenResponseModel(string token, DateTime expireDate)
+        {
+            Token = token;
+            ExpireDate = expireDate;
+        }
+
+        public string Token { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+}
