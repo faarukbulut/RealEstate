9027942525064479e4cfc7d4aa1dd5d05b82219e
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-27 10:52:51 +0300

    Token'ın UI Kısmında Login ile Kullanılması

---
+++ RealEstate_UI/Controllers/LoginController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_UI.Dtos.LoginDtos;
+using RealEstate_UI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+
+namespace RealEstate_UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public LoginController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ResultLoginDto resultLoginDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonSerializer.Serialize(resultLoginDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44367/api/Login", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = await responseMessage.Content.ReadAsStringAsync();
+                var tokenModel = JsonSerializer.Deserialize<JwtResponseModel>(responseData, new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                });
+
+                if(tokenModel != null)
+                {
+                    JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+                    var token = handler.ReadJwtToken(tokenModel.Token);
+                    var claims = token.Claims.ToList();
+
+                    if(tokenModel.Token != null)
+                    {
+                        claims.Add(new Claim("realestatetoken", tokenModel.Token));
+                        var claimsidentity = new ClaimsIdentity(claims, JwtBearerDefaults.AuthenticationScheme);
+                        var authProps = new AuthenticationProperties
+                        {
+                            ExpiresUtc = tokenModel.ExpireDate,
+                            IsPersistent = true
+                        };
+
+                        await HttpContext.SignInAsync(JwtBearerDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsidentity), authProps);
+                        return RedirectToAction("Index", "MyAdverts", new { area = "EstateAgent" });
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+
+
+            return View();
+        }
+    }
+}
---
+++ RealEstate_UI/Dtos/LoginDtos/ResultLoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate_UI.Dtos.LoginDtos
+{
+    public class ResultLoginDto
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
