fd4b68a4ccee1bfaa2338d1df8e8b5db6a421e76
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-04-06 01:46:46 +0300

    estate agent dashboard ek düzenleme

--- RealEstate_UI/Areas/EstateAgent/ViewComponents/EstateAgentPage/_EstateAgentDashboardChartComponentPartial.cs
+++ RealEstate_UI/Areas/EstateAgent/ViewComponents/EstateAgentPage/_EstateAgentDashboardChartComponentPartial.cs
@@ -17,14 +17,12 @@ namespace RealEstate_UI.Areas.EstateAgent.ViewComponents.EstateAgentPage
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:44367/api/Charts");
-
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<DashboardChartModel>>(jsonData);
                 return View(values);
             }
-
             return View();
         }
     }
